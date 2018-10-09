fed28820ab7e4ab96bc7055f34fc6597a37f84d6
mftw <michaelnlkoch@gmail.com>
2018-10-09 12:15:28 +0200

    added stuff

---
+++ src/app.js
@@ -0,0 +1,18 @@
+/**
+ * Installer service worker hvis nødvendigt
+ * Du skal referere til denne fil fra dit sites footer
+ * Bemærk at din service worker fil skal ligge på roden af dit site (Linie 8)
+ */
+if('serviceWorker' in navigator) {
+    window.addEventListener("load", function() {
+        navigator.serviceWorker.register('./service-worker.js')
+            .then(function (registration) {
+                console.log("Service Worker registred");
+            })
+            .catch(function(error) {
+                console.error("Registration failed!");
+            })
+    })
+}
+
+// console.log(filesToCache);
