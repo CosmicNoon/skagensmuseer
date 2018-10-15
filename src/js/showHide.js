a3cbfa7bed192348682ce2e5adbbc05ff7f4257d
mftw <michaelnlkoch@gmail.com>
2018-10-15 13:52:44 +0200

    icons and stuff

--- src/js/showHide.js
+++ src/js/showHide.js
@@ -4,17 +4,8 @@
  */
 function changePage(jumpTo) {
 
-    // Gør det nemmere at få fat i alle af en type element.
-    let $all = (element) => {
-        return document.querySelectorAll(element)
-    }
-
-    // Få menuen
-    // let menu = $('.menu');
-    let menu = document.querySelector('.menu');
-
     // Få siderne der skal skiftes imellem, som array (HTMLCollection)
-    let frames = $all('.frame');
+    let frames = document.querySelectorAll('.frame')
 
     // Få transisionstiden fra en af siderne (de er ens). 
     // Den returneres som sekunder men skal bruges som millisekunder
@@ -48,14 +39,20 @@ function changePage(jumpTo) {
     }
 
     // Sørg for at menuen altid er lukket når der skiftes side.
-    menu.classList.remove('show-menu');
+    showMenu('disable');
 
-    // Fjern størrelses begrænsninger på den viste side.
-    restrictSize('remove');
+    // Fjern størrelses begrænsninger på den viste side når menuen lukkes.
+    restrictSize('disable');
 
     // Bryd ud af funtionen hvis den viste side er den samme som den ønskede side
     if (currentShow == jumpTo) {
         return;
+    }
+
+    if(currentFrame.querySelector('video')) {
+        currentFrame.querySelectorAll('video').forEach(video => {
+            video.pause();
+        });
     }
 
     // Vis den ønskede side med ".show" klassen
@@ -64,74 +61,163 @@ function changePage(jumpTo) {
     // Sørg for den ønskede side altid ligger øverst med ".stack-top" klassen.
     showPage.classList.add('stack-top');
 
+    if (showPage.querySelector('.fade-to-white')) {
+        menuIcons.forEach(icon => {
+            icon.classList.add('icon-header--white')
+        });
+    } else {
+        menuIcons.forEach(icon => {
+            icon.classList.remove('icon-header--white')
+        });
+    }
+
     // Vent den tid det tager at skifte side (transitionTime)
     setTimeout(function () {
         // Fjern den viste side
         currentFrame.classList.remove('show');
 
         // Fjern ".stack-top" når den gamle side er væk.
-        showPage.classList.remove('stack-top');
+        // showPage.classList.remove('stack-top');
 
         // Brug den transisionstid vi fik fra css klassen.
     }, transitionTime);
+
+    // Vent den tid det tager at skifte side (transitionTime)
+    setTimeout(function () {
+        // Fjern den viste side
+        // currentFrame.classList.remove('show');
+
+        // Fjern ".stack-top" når den gamle side er væk.
+        showPage.classList.remove('stack-top');
+
+        // Brug den transisionstid vi fik fra css klassen.
+    }, transitionTime * 2);
 }
 
 
-function showMenu() {
+function showMenu(remove) {
     let menu = document.querySelector('.menu');
-    menu.classList.toggle('show-menu');
+
+    if (remove === 'disable') {
+        menu.classList.remove('show-menu');
+    } else {
+        menu.classList.toggle('show-menu');
+    }
+    
+    // console.log(menuBtns);
     restrictSize();
 }
 
 function restrictSize(remove) {
     let currentShow = document.querySelector('.show');
-    if(remove === 'remove'){
+    if (remove === 'disable') {
         currentShow.classList.remove('restrict-size')
     } else {
         currentShow.classList.toggle('restrict-size')
     }
 }
 
-// var scrollPosition = document.documentElement.scrollTop || document.body.scrollTop || window.pageYOffset;
-// var scrollPosition = Math.max(document.documentElement.scrollTop, document.body.scrollTop, window.pageYOffset);
-// console.log(scrollPosition);
-
 var headers = document.querySelectorAll('.frame header');
 var headerHeight = headers[0].offsetHeight;
-console.log(headerHeight);
-
 var frameContainer = document.querySelector('.frame-container');
 
-// frameContainer.onscroll = function () {
-//     console.log(frameContainer.scrollTop);
+// let headerContent = document.querySelectorAll('.frame header *');    
+// let contentTop = parseFloat(getComputedStyle(headerContent[0])['top']);
 
-//     headers.forEach(header => {
-//         header.style.willChange = "height";
-        
-//         if(frameContainer.scrollTop > (headerHeight / 2)) {
-//             frameContainer.style.height = 'calc(100% - 40px)';
-//             header.classList.add('shrink');
-//             // frameContainer.style.marginBottom = -(headerHeight / 2);
-            
-//         } else {
-//             header.classList.remove('shrink');
-//             frameContainer.removeAttribute('style');
-//         }
-//         // setTimeout(function() {
+// let scale = 
 
-//         // }, 500);
-//         // if(frameContainer.scrollTop > (headerHeight / 2)) {
-//         //     header.style.height = headerHeight / 2 + 'px';
-//         // } else {
-//         //     header.style.height = headerHeight - frameContainer.scrollTop + 'px';
-//         // }
-        
-//     });
+// let contentHeight = headerContent[0].offsetHeight;
+var scrollPos = frameContainer.scrollTop;
+var fadePos = headerHeight - (headerHeight / 2) + 10;
+var fadeOffset = 0;
+var menuIcons = document.querySelectorAll('.icon-header');
+// var logoSVG = document.getElementById('logo-svgID');
+// var menuSVG = document.getElementById('menu-header-svgID');
+// console.log(menuSVG);
+// var showedHeader = null;
+frameContainer.onscroll = function () {
+    // let dir = '';
+    // if (scrollPos < frameContainer.scrollTop) {
+    //     dir = 'down';
+    // } else {
+    //     dir = 'up';
+    // }
+    // console.log(dir);
+
+    scrollPos = frameContainer.scrollTop;
+    var showedFrame = document.querySelector('.frame.show')
+    // var showedHeader = document.querySelector('.frame.show .moving-header');
+    var showedHeader = showedFrame.querySelector('.moving-header');
+    // showedHeaderHeight = showedHeader.offsetHeight;
+
+    // console.log(showedHeader);
+    // if (showedHeader.classList.contains('show')) {
+
+    // }
+
+    if (scrollPos > (headerHeight / 2)) {
+        showedHeader.style.height = headerHeight / 2 + 'px';
+
+    } else {
+        showedHeader.style.height = headerHeight - (scrollPos) + 'px';
+        // content.style.top = contentTop - (scrollPos / 2.5) + 'px';
+
+    }
+
+    if (showedFrame.querySelector('.fade-to-white')) {
+        if (scrollPos > (headerHeight / 2) + 20) {
+            // if (showedHeader.classList.contains('fade-to-white')){
+
+            // }
+            // showedFrame.querySelector('.fade-to-white').style.background = 'rgba(255,255,255,1)'
+            showedFrame.querySelector('.fade-to-white').classList.add('active');
+            // menuSVG.classList.remove('icon-header-menu--white');
+            // logoSVG.classList.remove('logo-svg--white');
+            menuIcons.forEach(icon => {
+                icon.classList.remove('icon-header--white')
+            });
+        } else {
+            showedFrame.querySelector('.fade-to-white').classList.remove('active');
+            // menuSVG.classList.add('icon-header-menu--white');
+            // logoSVG.classList.add('logo-svg--white');
+            menuIcons.forEach(icon => {
+                icon.classList.add('icon-header--white')
+            });
+        }
+    }
+}
+
+var currentFrame = document.querySelector('.show');
+
+if (currentFrame.querySelector('.fade-to-white')) {
+    // menuSVG.classList.add('icon-header-menu--white');
+    menuIcons.forEach(icon => {
+        icon.classList.add('icon-header--white')
+    });
+} else {
+    // menuSVG.classList.remove('icon-header-menu--white')
+    menuIcons.forEach(icon => {
+        icon.classList.remove('icon-header--white')
+    });
+}
+
+// var navLinks = document.querySelectorAll('.navbar a');
+// navLinks.forEach(link => {
+//     link.onclick = 
+// });
+
+// var frames = document.querySelectorAll('.frame');
+
+// for (let i = 0; frames.length; i++) {
+//     if(frames[i].classList.contains('show')) {
+//         break;
+//     } 
+//     if(i = frames.length && frames[i].classList.contains('show') != true) {
+//         frames[0].classList.add('show');
+//     }
 // }
 
 
-
-
-
-
-
+// console.log(document.querySelectorAll('.frame header *'));
+// console.log(test);
+// console.log('asdfasdfasdfasdf');
